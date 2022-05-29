66c1b74eb901feff4d11faa11250a772d36a9186
rifatx <rifat.aricanli@gmail.com>
2022-05-29 12:01:49 +0300

    add other project templates

---
+++ src/rider/main/resources/projectTemplates/NFClassLibraryTemplate/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace __NFProject__
+{
+    public class NFClassLibrary1
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
---
+++ src/rider/main/resources/projectTemplates/NFUnitTestTemplate/UnitTest1.cs
@@ -0,0 +1,14 @@
+using nanoFramework.TestFramework;
+using System;
+
+namespace __NFProject__
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
